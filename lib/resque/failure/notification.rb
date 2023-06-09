835e9bc1684e01d36c3c80c8dfd4720e2cb64d86
Matthew Hively <matthewhively@viz.com>
2023-06-09 15:23:16 -0700

    Update variable name

--- lib/resque/failure/notification.rb
+++ lib/resque/failure/notification.rb
@@ -29,8 +29,8 @@ module Resque
       # Returns the formatted payload linked to the failed job
       #
       def msg_payload
-        backtrace = @failure.payload.inspect.split('\n') if @failure&.payload&.inspect
-        "Payload:\n#{format_message(backtrace)}"
+        payload = @failure.payload.inspect.split('\n') if @failure&.payload&.inspect
+        "Payload:\n#{format_message(payload)}"
       end
 
       # Returns the formatted exception linked to the failed job
